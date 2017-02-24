531acbc4523f2d8ab0ab33f7141671eef5ba342c
Haripriya Mehta <haripriyamehta@dhcp-18-189-30-58.dyn.mit.edu>
2017-02-24 11:47:23 -0500

    Added GeoQuiz

---
+++ GeoQuiz/app/src/main/java/com/example/haripriyamehta/geoquiz/TrueFalse.java
@@ -0,0 +1,24 @@
+package com.example.haripriyamehta.geoquiz;
+
+/**
+ * Created by haripriyamehta on 2/23/17.
+ */
+
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+}
